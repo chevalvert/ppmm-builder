a446a99c0ed681042a3bec7de872648be40464bc
RNO <arnaud.juracek@gmail.com>
2020-01-24 16:26:52 +0100

    Update test

--- test/index.js
+++ test/index.js
@@ -1,10 +1,25 @@
 const tmp = require('tmp')
 const path = require('path')
 const fs = require('fs-extra')
+const bounds = require('geojson-bounds')
+
 const pkg = require('../package.json')
 const build = require('../lib')
 
+const PATHS = {
+  geojson: path.join(__dirname, 'sample.geojson'),
+  style: path.join(__dirname, 'style.json')
+}
+
+// NOTE: automatically computing the geojson bbox of the sample file to simplify
+// on-the-fly edits of the sample. This of course defeats the principle of using
+// streams to avoid memory-intensive computation, but this is a test with a
+// small sample.
+const geojson = fs.readJsonSync(PATHS.geojson, 'utf8')
+const boundingBox = bounds.extent(geojson)
+
 const options = {
+  boundingBox,
   verbose: false,
   output: tmp.dirSync({ prefix: pkg.name + '__', keep: true }).name,
   root: path.join(__dirname),
@@ -19,8 +34,8 @@ const options = {
     console.time(pkg.name)
 
     const { files, warnings } = await build(
-      fs.createReadStream(path.join(__dirname, 'sample.geojson')),
-      await fs.readJson(path.join(__dirname, 'style.json'), 'utf8'),
+      fs.createReadStream(PATHS.geojson),
+      await fs.readJson(PATHS.style, 'utf8'),
       options
     )
 
